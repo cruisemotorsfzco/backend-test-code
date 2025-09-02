f581566cfd5389d0a718910ce82ef8fd42664056
Vaibhav <vaibhavdadhaniya369@gmail.com>
2025-09-02 17:38:00 +0530

    feat/posts only updated and deleted by authors and roleguard added

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Param, Patch, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, UseGuards, Req } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/strategy/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/strategy/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/shared/enums/user-roles.enum';
 
 @ApiTags('posts')
 @Controller('posts')
@@ -16,11 +19,13 @@ export class PostsController {
     @Post()
     @ApiOperation({ summary: 'Create a new post' })
     @ApiResponse({ status: 201, description: 'Post created successfully' })
-    create(@Body() dto: CreatePostDto) {
-        return this.postsService.create(dto);
+    create(@Body() dto: CreatePostDto, @Req() req: any) {
+        const user = req.user as { userId: string };
+        return this.postsService.create(dto, user.userId);
     }
 
     @UseGuards(JwtAuthGuard)
+    // @Roles(Role.ADMIN, Role.USER)
     @ApiBearerAuth()
     @Get()
     @ApiOperation({ summary: 'Get all posts (with author)' })
@@ -30,6 +35,7 @@ export class PostsController {
     }
 
     @UseGuards(JwtAuthGuard)
+    // @Roles(Role.ADMIN, Role.USER)
     @ApiBearerAuth()
     @Get(':id')
     @ApiOperation({ summary: 'Get a post by ID' })
@@ -39,20 +45,22 @@ export class PostsController {
     }
 
     @UseGuards(JwtAuthGuard)
+    // @Roles(Role.ADMIN, Role.USER)
     @ApiBearerAuth()
     @Patch(':id')
     @ApiOperation({ summary: 'Update a post by ID' })
     @ApiResponse({ status: 200, description: 'Post updated successfully' })
-    update(@Param('id') id: string, @Body() dto: UpdatePostDto) {
-        return this.postsService.update(id, dto);
+    update(@Param('id') id: string, @Body() dto: UpdatePostDto, @Req() req: any) {
+        return this.postsService.update(id, dto, req.user.userId);
     }
 
     @UseGuards(JwtAuthGuard)
+    // @Roles(Role.ADMIN, Role.USER)
     @ApiBearerAuth()
     @Delete(':id')
     @ApiOperation({ summary: 'Delete a post by ID' })
     @ApiResponse({ status: 200, description: 'Post deleted successfully' })
-    remove(@Param('id') id: string) {
-        return this.postsService.remove(id);
+    remove(@Param('id') id: string, @Req() req: any) {
+        return this.postsService.remove(id, req.user.userId);
     }
 }
